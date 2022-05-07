dd5def6d391b6142a2447f8d8b599d0f237464b0
mazyn <gilmarfilho75@gmail.com>
2022-05-07 12:29:22 -0300

    web(class-5): part 1 - web final touches

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from 'phosphor-react'
 import { FormEvent, useState } from 'react'
 import { FeedbackType, feedbackTypes } from '..'
+import { api } from '../../../lib/api'
 import { CloseButton } from '../../CloseButton'
+import { Loading } from '../../Loading'
 import { ScreenshotButton } from '../ScreenshotButton'
 
 export interface IFeedbackContentStepProps {
@@ -17,16 +19,24 @@ export function FeedbackContentStep({
 }: IFeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
-  function handleSubmitFeedback(e: FormEvent) {
+  async function handleSubmitFeedback(e: FormEvent) {
     e.preventDefault()
 
-    console.log({
-      screenshot,
-      comment
+    if (isSendingFeedback) return
+
+    setIsSendingFeedback(true)
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
     })
+
+    setIsSendingFeedback(false)
 
     onFeedbackSent()
   }
@@ -68,9 +78,9 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={!comment?.trim()}
+            disabled={!comment?.trim() || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
